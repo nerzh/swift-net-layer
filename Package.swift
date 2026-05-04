6a66d1687e5be6d17b9f3ee456c0b1da78e1546d
nerzh <emptystamp@gmail.com>
2026-05-04 06:36:53 +0200

    fix dep

--- Package.swift
+++ Package.swift
@@ -17,13 +17,15 @@ let package = Package(
     
     dependencies: [
         .package(url: "https://github.com/nerzh/swift-extensions-pack.git", .upToNextMajor(from: "2.0.0")),
+        .package(url: "https://github.com/nerzh/swift-regular-expression.git", .upToNextMajor(from: "0.2.4")),
     ],
     
     targets: [
         .target(
             name: "SwiftNetLayer",
             dependencies: [
-                .product(name: "SwiftExtensionsPack", package: "swift-extensions-pack")
+                .product(name: "SwiftExtensionsPack", package: "swift-extensions-pack"),
+                .product(name: "SwiftRegularExpression", package: "swift-regular-expression")
             ])
     ]
 )
